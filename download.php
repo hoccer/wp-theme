58ee89db96f8a61ac31e077480e59cd4d08f04ef
zutrinken <xoxo@peteramende.de>
2013-08-28 18:44:39 +0200

    set redirects temporaly

--- download.php
+++ download.php
@@ -10,23 +10,23 @@
 	if($iPod || $iPhone || $iPad) {
 		$appstore = get_post_meta($post->ID, 'appstore', true);
 		if ($appstore) {
-			header('Location: '. $appstore);
+			header('Location: '. $appstore, true, 307);
 		} else {
-			header('Location: '. $home);
+			header('Location: '. $home, true, 307);
 		}
 	} else if($Android) {
 		$playstore = get_post_meta($post->ID, 'playstore', true);
 		if ($playstore) {
-			header('Location: '. $playstore);
+			header('Location: '. $playstore, true, 307);
 		} else {
-			header('Location: '. $home);
+			header('Location: '. $home, true, 307);
 		}
 	} else {
 		$default = get_post_meta($post->ID, 'default', true);
 		if ($default) {
-			header('Location: '. $default);
+			header('Location: '. $default, true, 307);
 		} else {
-			header('Location: '. $home);
+			header('Location: '. $home, true, 307);
 		}
 	}
 ?> 
