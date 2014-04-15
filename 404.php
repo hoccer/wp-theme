feb959bc7cafd84e2b3e3d7dc9784a700437a1bc
zutrinken <xoxo@peteramende.de>
2014-04-15 10:44:22 +0200

    added 404 page

---
+++ 404.php
@@ -0,0 +1,20 @@
+		<?php get_header(); ?>
+		<div id="header-teaser">
+			<div id="header-teaser-outside">
+				<div class="header-teaser-inside inner">
+					<h1 class="post-title"><?php _e('404','hoccer'); ?></h1>
+				</div>
+			</div>
+		</div>
+		<div id="wrapper">
+			<div class="inner">
+				<section id="container">
+					<section id="post-404" class="type-page">
+						<article class="article">
+							<p><?php _e('Page not found.','hoccer'); ?></p>
+						</article>
+					</section>	
+				</section>
+			</div>
+		</div>
+		<?php get_footer(); ?>
